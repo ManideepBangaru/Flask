1988d72ce8e6acf7203e1f2b158c1db2960be3d0
ManideepBangaru <bmd994@gmail.com>
2022-05-17 00:20:25 +0530

    added if and for loops using jinja coding

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
+from functools import update_wrapper
 from flask import Flask, render_template
+from pkg_resources import safe_extra
 
 # create a flask instance
-
 app = Flask(__name__)
 
 
 # create a route decorator
-
 @app.route('/')
 
 # def index():
 #     return "<h1> Hello World </h1>"
 
+# safe
+# capitalize
+# upper
+# lower
+# title
+# striptags
+
 def index():
-    return render_template("home.html")
+    my_name = "Manideep"
+    stuff = "<p> This is a <strong> bold </strong> text </p>"
+    favoritePizza = ["pepperoni","cheeseburst","barbecue",99]
+    return render_template("home.html",
+    first_name = my_name,
+    stuff=stuff,
+    favoritePizza = favoritePizza)
 
 # /localhost:5000/user/manideep
 @app.route('/user/<name>')
 def user(name):
-    return "<h1> Hello %s !!!</h1>"%name
+    return render_template("user.html",user_name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)

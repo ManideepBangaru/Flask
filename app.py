1886d3624a68a16c25e4e9c56c4231c9f913c44e
ManideepBangaru <bmd994@gmail.com>
2022-05-16 23:36:40 +0530

    made small edits

--- app.py
+++ app.py
@@ -14,5 +14,8 @@ def index():
 
 # /localhost:5000/user/manideep
 @app.route('/user/<name>')
-def index():
-    return "<h1> Hello </h1>"
+def user(name):
+    return "<h1> Hello %s !!!</h1>"%name
+
+if __name__ == "__main__":
+    app.run(debug=True)
